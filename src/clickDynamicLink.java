d9c5cbcb63486192969becb5b504e33c6c99ac4b
oktaviadpp <oktaviadwiputri506@gmail.com>
2025-01-08 15:02:35 +0700

    add dynamic link

--- src/clickDynamicLink.java
+++ src/clickDynamicLink.java
@@ -1,4 +1,8 @@
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,14 +11,31 @@ public class clickDynamicLink {
 
 	public static void main(String[] args) {
 		// class ini contoh test click href/link dynamic
-		
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
-		//count link on footer
+
+		// count link on footer
 		WebElement footerdriver = driver.findElement(By.id("gf-BIG"));
 		WebElement columndriver = footerdriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
-		System.out.println("jumlah link = "+footerdriver.findElements(By.tagName("a")).size());
+		System.out.println("jumlah link = " + columndriver.findElements(By.tagName("a")).size());
+
+		// click dynamic footer
+		for (int i = 1; i < columndriver.findElements(By.tagName("a")).size(); i++) {
+			String clickonlinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+			columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
+		}
+
+		// get title from all tab
+		Set<String> windowHandle = driver.getWindowHandles();
+		Iterator<String> it = windowHandle.iterator();
+
+		while (it.hasNext()) {
+
+			driver.switchTo().window(it.next());
+			System.out.println(driver.getTitle());
+
+		}
 
 	}
 
